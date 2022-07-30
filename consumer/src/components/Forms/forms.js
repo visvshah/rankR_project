c111c6ad1152db5ccbddcd375bac5eb3bdd3f52d
VisvShah <visvshah1@gmail.com>
2022-07-29 23:36:00 -0400

    Finished Redux
    -Finished Redux
    -Worked on front end of form

--- consumer/src/components/Forms/forms.js
+++ consumer/src/components/Forms/forms.js
@@ -1,7 +1,28 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import { TextField, Button, Typography, Paper, ListItemSecondaryAction} from '@material-ui/core'
+import FileBase from 'react-file-base64';
+import './forms.scss'
 export default function forms() {
+  const [listData, setListData] = useState({
+    author: '', title: '', content: '', tags: ''
+  });
+  const handleSubmit = () =>{
+
+  }
+  const clear = ()=>{
+
+  }
   return (
-    <div>forms</div>
+    <Paper className = "paper">
+      <form autoComplete = "off" Validate className = 'form' onSumbit = {handleSubmit}>
+        <Typography variant = "h6">Create a List</Typography>
+        <TextField className = 'input' name = "title" variant = "outlined"  label = "Title"  fullWidth value = {listData.title} onChange = {(event) => setListData({...listData, title: event.target.value})}/>
+        <TextField className = 'input' name = "content" variant = "outlined"  label = "Content"  fullWidth value = {listData.content} onChange = {(event) => setListData({...listData, content: event.target.value})}/>
+        <TextField className = 'input' name = "author" variant = "outlined"  label = "Author"  fullWidth value = {listData.author} onChange = {(event) => setListData({...listData, author: event.target.value})}/>
+        <TextField className = 'input' name = "tags" variant = "outlined"  label = "Tags"  fullWidth value = {listData.tags} onChange = {(event) => setListData({...listData, tags: event.target.value})}/>
+        <Button className ="buttonSubmit" variant = "contained" color = "primary" size = "large" type = "submit" fullWidth>Submit</Button>
+        <Button variant = "contained" color = "secondary" size = "small" OnClick={clear} fullWidth>Clear</Button>
+      </form>
+    </Paper>
   )
 }
--- server/index.js
+++ server/index.js
@@ -5,11 +5,12 @@ import cors from "cors";
 import listRoutes from "./routes/lists.js"
 const app = express();
 
-app.use('/lists', listRoutes)
+
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+app.use('/lists', listRoutes)
 
 const CONNECTION_URL = 'mongodb+srv://VisvShah:V123321@cluster0.ibin7vz.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 4000;
