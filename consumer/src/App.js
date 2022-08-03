2bd8f40b3d940a636fe8a0e28f840a351910b0df
VisvShah <visvshah1@gmail.com>
2022-08-03 14:45:57 -0400

    Bug Fix

--- consumer/src/App.js
+++ consumer/src/App.js
@@ -1,35 +1,15 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import "./app.scss"
-import {Container, Grow, Grid} from '@material-ui/core'
-import { useDispatch } from 'react-redux'
-import {getLists} from './actions/lists.js'
-import RankRLogo from "./images/RankRLogo.png"
-import Lists from "./components/Lists/lists.jsx"
+import {Container} from '@material-ui/core'
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
 import Navbar from "./components/Navbar/navbar.jsx"
-import Form from "./components/Forms/forms.jsx"
+import Home from "./components/Home/home.jsx"
+
 export default function App() {
-  const [currentId, changeId] = useState(0);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getLists());
-  },[currentId, dispatch])
-  
   return (
       <Container className = "app" maxwidth="lg">
         <Navbar/>
-        <Grow in>
-          <Container className ="Content">
-            <Grid container justifyContent = "space-between" alignItems = "stretch" spacing={3}>
-              <Grid className="Posts" item xs={12} sm={7}>
-                <Lists changeId = {changeId}/>
-              </Grid>
-              <Grid className="Form" item xs={12} sm={4}>
-                <Form currentId = {currentId} changeId = {changeId}/>
-              </Grid>
-            </Grid>
-
-          </Container>
-        </Grow>
+        <Home/>
       </Container>
   )
 }
--- consumer/src/components/Home/home.jsx
+++ consumer/src/components/Home/home.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Container, Grow, Grid} from '@material-ui/core'
 import Form from "../Forms/forms.jsx"
-import Lists from "./components/Lists/lists.jsx"
+import Lists from "../Lists/lists.jsx"
+import { useDispatch } from 'react-redux'
+import {getLists} from '../../actions/lists.js'
 
 export default function home() {
-  return (
-    <Grow in>
-        <Container className ="Content">
-            <Grid container justifyContent = "space-between" alignItems = "stretch" spacing={3}>
-                <Grid className="Posts" item xs={12} sm={7}>
-                    <Lists changeId = {changeId}/>
+    const [currentId, changeId] = useState(0);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getLists());
+    },[currentId, dispatch])
+  
+    return (
+        <Grow in>
+            <Container className ="Content">
+                <Grid container justifyContent = "space-between" alignItems = "stretch" spacing={3}>
+                    <Grid className="Posts" item xs={12} sm={7}>
+                        <Lists changeId = {changeId}/>
+                    </Grid>
+                    <Grid className="Form" item xs={12} sm={4}>
+                        <Form currentId = {currentId} changeId = {changeId}/>
+                    </Grid>
                 </Grid>
-                <Grid className="Form" item xs={12} sm={4}>
-                    <Form currentId = {currentId} changeId = {changeId}/>
-                </Grid>
-            </Grid>
-        </Container>
-    </Grow>
-  )
+            </Container>
+        </Grow>
+    )
 }
